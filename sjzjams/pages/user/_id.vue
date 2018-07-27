26a3cac258879b5e88659d9c65b9858af747d2d1
sjzjams <sjzjams@gmail.com>
2018-07-27 17:23:54 +0800

    增加动态路由、axios.all
    增加图书详情动态路由，同时请求两个url。

---
+++ sjzjams/pages/user/_id.vue
@@ -0,0 +1,38 @@
+<template>
+ <section class="container">
+     <div class="content">
+    <h1 class="red">标题 {{ title }}</h1> 
+    <span>
+        作者:{{author}}
+    </span>
+     <img :src="image"/>
+    <h2>
+      简介：{{summary}}
+    </h2>
+     </div>
+     <div>
+       <span>项目名称：{{productName}}</span>
+       <a href="/" class="button--green">返回首页</a>
+     </div>
+ </section>
+
+  
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data(){
+   return { productName: 'world' }
+  },
+  //调用豆瓣api
+  async asyncData ({ params }) {
+      console.log(params);
+    let {data}= await  axios.get('https://api.douban.com/v2/book/'+params.id+'')
+    return { title:data.title, author: data.author,summary:data.summary,image:data.image}
+  },
+}
+</script>
+
+
+
--- sjzjams/pages/user/index.vue
+++ sjzjams/pages/user/index.vue
@@ -1,14 +1,27 @@
 <template>
  <section class="container">
      <div class="content">
-    <h1 class="red">标题 {{ title }}</h1> 
+    <h1 class="red"><a href="/user/1220562" class="button--green">标题 {{ title }}</a></h1> 
     <span>
         作者:{{author}}
     </span>
-     <img :src="image">
+     <img :src="image"/>
     <h2>
       简介：{{summary}}
     </h2>
+     </div>
+     <div class="content">
+    <h1 class="red">标题1 {{ title1 }}</h1> 
+    <span>
+        作者1:{{author1}}
+    </span>
+     <img :src="image1"/>
+    <h2>
+      简介1：{{summary1}}
+    </h2>
+     </div>
+     <div>
+       <span>项目名称：{{productName}}</span>
      </div>
  </section>
 
@@ -18,14 +31,29 @@
 <script>
 import axios from 'axios'
 export default {
-  data () {
-    return { name: 'world' }
+  data(){
+   return { productName: 'world' }
   },
   //调用豆瓣api
   async asyncData ({ params }) {
-    let { data } = await axios.get('https://api.douban.com/v2/book/1220562')
-    return { title:data.title, author: data.author,summary:data.summary,image:data.image }
-  }
+    var that=this
+    return axios.all([
+      axios.get('https://api.douban.com/v2/book/1220562'),
+      axios.get('https://api.douban.com/v2/book/1220561')])
+      .then(axios.spread(function (book1,book2) {
+        console.log('book1',book1.data);
+        return { 
+          title:book1.data.title, 
+          author: book1.data.tags[0].name,
+          summary:book1.data.summary,
+          image:book1.data.image ,
+          title1:book2.data.title, 
+          author1: book2.data.tags[0].name,
+          summary1:book2.data.summary,
+          image1:book2.data.image ,
+          }
+      }))
+  },
 }
 </script>
 
